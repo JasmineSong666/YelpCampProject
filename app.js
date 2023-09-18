73a5bdd9ac32838f79178558a5a96d383fff769f
Song <jzsminesong@gmail.com>
2023-09-18 12:42:06 +0800

    change port

--- app.js
+++ app.js
@@ -172,7 +172,8 @@ app.use((err, req, res, next) => {
 
 })
 
-app.listen(3000, () => {
-    console.log("listening on port 3000")
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
 
